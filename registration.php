7657cc3ae82100a4f5ea9e6b9ca0d202a2ae512f
Ingenico ePayments <github@epay.ingenico.com>
2023-04-19 10:12:26 +0200

    Release 3.0.1.

--- registration.php
+++ registration.php
@@ -2,9 +2,4 @@
 
 use Magento\Framework\Component\ComponentRegistrar;
 
-ComponentRegistrar::register(
-    ComponentRegistrar::MODULE,
-    'Worldline_Connect',
-    // phpcs:ignore Generic.PHP.ForbiddenFunctions.Found
-    isset($file) ? dirname($file) : __DIR__
-);
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Worldline_Connect', __DIR__);
